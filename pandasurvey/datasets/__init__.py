3f94aa2516f4c295ef0f2bb44201df3bd66a7276
Victor Brakauskas <victor.brakauskas@incontextsolutions.com>
2014-08-27 17:42:16 -0500

    mid working

--- pandasurvey/datasets/__init__.py
+++ pandasurvey/datasets/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pandas
+from collections import defaultdict
 
 THIS_DIRECTORY, _ = os.path.split(__file__)
 
@@ -7,3 +8,13 @@ THIS_DIRECTORY, _ = os.path.split(__file__)
 def load_people():
     path = os.path.join(THIS_DIRECTORY, "People.csv")
     return pandas.read_csv(path)
+
+def load_rengine_weights():
+    path = os.path.join(THIS_DIRECTORY, "study1614,csv")
+    return pandas.read_csv(path)
+
+def load_target_weights():
+    path = os.path.join(THIS_DIRECTORY, "target_weights_1614.csv")
+    targets = pandas.read_csv(path)
+    ret_tar = {}
+    pass
