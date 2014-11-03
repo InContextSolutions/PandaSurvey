78acba7d1b976a21325611b950e225f48bcbcde2
Tristan Wietsma <tristan.wietsma@incontextsolutions.com>
2014-11-03 12:02:37 -0600

    rename

--- pandasurvey/mixins/recode.py
+++
@@ -1,8 +0,0 @@
-
-
-class RecodeMixin:
-
-    def recode(self):
-        for rec in self.recodes:
-            self.df[rec] = self.df.apply(
-                lambda row: self.recodes[rec](row[rec]), axis=1)
