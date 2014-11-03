adec802789366454d58c7b7378edb63b7afe3ba6
Tristan Wietsma <tristan.wietsma@incontextsolutions.com>
2014-11-03 14:36:41 -0600

    added docs

--- PandaSurvey/base.py
+++ PandaSurvey/base.py
@@ -3,6 +3,8 @@ from abc import ABCMeta, abstractmethod
 
 class SurveyWeightBase:
 
+    """Abstract base class for survey weighting method."""
+
     __metaclass__ = ABCMeta
 
     def __init__(self, *args, **kwargs):
@@ -10,8 +12,10 @@ class SurveyWeightBase:
 
     @abstractmethod
     def calc(self):
+        """Calculation step must be implemented here."""
         pass
 
     def loss(self, weights):
+        """Describes the inflation in the variance of sample estimates that can be attributed to weighting. See *Applied Survey Data Analysis* (2010) by Heeringa et al. for more information."""
         n = len(weights)
         return ((weights ** 2).sum() / weights.sum() ** 2) * n - 1
